d5e5da91877e60ecea3bc42d922beb3277eccf33
Corva_Tile <dogelord0908@gmail.com>
2024-02-21 17:12:12 -0500

    Interview Stage Wip Im a little tired rn

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -230,6 +230,7 @@ class PlayState extends MusicBeatState
 	public var inCutscene:Bool = false;
 	public var skipCountdown:Bool = false;
 	var songLength:Float = 0;
+	public var barSongLength:Float = 0;
 
 	public var boyfriendCameraOffset:Array<Float> = null;
 	public var opponentCameraOffset:Array<Float> = null;
@@ -381,14 +382,7 @@ class PlayState extends MusicBeatState
 		switch (curStage)
 		{
 			case 'stage': new states.stages.StageWeek1(); //Week 1
-			case 'spooky': new states.stages.Spooky(); //Week 2
-			case 'philly': new states.stages.Philly(); //Week 3
-			case 'limo': new states.stages.Limo(); //Week 4
-			case 'mall': new states.stages.Mall(); //Week 5 - Cocoa, Eggnog
-			case 'mallEvil': new states.stages.MallEvil(); //Week 5 - Winter Horrorland
-			case 'school': new states.stages.School(); //Week 6 - Senpai, Roses
-			case 'schoolEvil': new states.stages.SchoolEvil(); //Week 6 - Thorns
-			case 'tank': new states.stages.Tank(); //Week 7 - Ugh, Guns, Stress
+			case 'dawko': new states.stages.Dawko();
 		}
 
 		if(isPixelStage) {
--- source/states/stages/Dawko.hx
+++ source/states/stages/Dawko.hx
@@ -1,23 +1,30 @@
 package states.stages;
 
-import states.stages.objects.*;
+import backend.BaseStage;
 
 class Dawko extends BaseStage {
 	public var wall:FlxSprite;
     public var decor:FlxSprite;
+	public var lighting:FlxSprite;
+	public var mic_foreground:FlxSprite;
+	public var table:FlxSprite;
 
 	public override function create() {
 	{
-		add(wall = new FlxSprite(0, 0, Paths.image("stages/interview/backwall")));
-		wall.scale.set(0,0);
-
-		add(decor = new FlxSprite(0, 0, Paths.image("stages/interview/backwall")));
-		decor.scale.set(0,0);
+		add(wall = new FlxSprite(0, 0, Paths.image("stages/dawko/backwall")));
+		wall.scale.set(1.5,1.5);
+		add(decor = new FlxSprite(0, -300, Paths.image("stages/dawko/bg_decor")));
+		decor.scale.set(1.3,1.3);
+		add(table = new FlxSprite(1150,740, Paths.image("stages/dawko/table")));
+		table.scale.set(1.4,1.4);
 	}
 }
 	
-	override function createPost()
-	{
-		// Use this function to layer things above characters!
+	override function createPost() {
+		add(lighting = new FlxSprite(100,-250, Paths.image("stages/dawko/lighting")));
+		lighting.scale.set(1.4,1.4);
+		add(mic_foreground = new FlxSprite(1600,500, Paths.image("stages/dawko/mic_foreground")));
+		mic_foreground.scale.set(1.4,1.4);
+		mic_foreground.scrollFactor.set(0.85,0.85);
 	}
 }	
